f791d7d8aec7cee1194ffec168852470897c7153
Jose Rogerio de Almeida Junior <jose.junior@andritz.com>
2023-09-09 20:43:26 -0300

    feat(apartments): create apartment entity

---
+++ src/Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,30 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Currency
+{
+    public static readonly Currency Usd = new("USD");
+    public static readonly Currency Eur = new("EUR");
+    public static readonly Currency Brl = new("BRL");
+
+    internal static readonly Currency None = new(string.Empty);
+
+    public static readonly IReadOnlyCollection<Currency> All = new[]
+    {
+        Usd,
+        Eur,
+        Brl
+    };
+
+    private Currency(string code)
+    {
+        Code = code;
+    }
+
+    public string Code { get; init; }
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(c => c.Code == code) ??
+               throw new ApplicationException("The currency code is invalid");
+    }
+}
---
+++ src/Bookify.Domain/Apartments/Money.cs
@@ -0,0 +1,29 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Money(decimal Amount, Currency Currency)
+{
+    public static Money operator +(Money first, Money second)
+    {
+        if (first.Currency != second.Currency)
+        {
+            throw new InvalidOperationException("Currencies have to be equal");
+        }
+
+        return first with { Amount = first.Amount + second.Amount };
+    }
+
+    public static Money Zero()
+    {
+        return new Money(0, Currency.None);
+    }
+
+    public static Money Zero(Currency currency)
+    {
+        return new Money(0, currency);
+    }
+
+    public bool IsZero()
+    {
+        return this == Zero(Currency);
+    }
+}
