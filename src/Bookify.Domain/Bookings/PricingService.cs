2c8a2dd1881cb70710e88f6132367039926c6bcf
Jose Rogerio de Almeida Junior <jose.junior@andritz.com>
2023-09-09 23:32:23 -0300

    feat(bookings): create booking entity

---
+++ src/Bookify.Domain/Bookings/PricingService.cs
@@ -0,0 +1,28 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Shared;
+
+namespace Bookify.Domain.Bookings;
+
+public sealed class PricingService
+{
+    public PricingDetails CalculatePrice(Apartment apartment, DateRange period)
+    {
+        var priceForPeriod = apartment.Price with { Amount = apartment.Price.Amount * period.LengthInDays };
+
+        var percentageUpCharge = apartment.Amenities.Sum(amenity => amenity switch
+        {
+            Amenity.GardenView or Amenity.MountainView => 0.05m,
+            Amenity.AirConditioning => 0.01m,
+            Amenity.Parking => 0.01m,
+            _ => 0
+        });
+
+        var amenitiesUpCharge = percentageUpCharge > 0
+            ? apartment.Price with { Amount = priceForPeriod.Amount * percentageUpCharge }
+            : Money.Zero(apartment.Price.Currency);
+
+        var totalPrice = priceForPeriod + apartment.CleaningFee + amenitiesUpCharge;
+
+        return new PricingDetails(priceForPeriod, apartment.CleaningFee, amenitiesUpCharge, totalPrice);
+    }
+}
--- src/Bookify.Domain/Shared/Money.cs
+++ src/Bookify.Domain/Shared/Money.cs
@@ -1,4 +1,4 @@
-﻿namespace Bookify.Domain.Apartments;
+﻿namespace Bookify.Domain.Shared;
 
 public sealed record Money(decimal Amount, Currency Currency)
 {
