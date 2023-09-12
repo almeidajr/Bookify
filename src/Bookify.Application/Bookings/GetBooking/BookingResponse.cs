8201677b2229a9bb637928540314f6bd0a7072b1
Jose Rogerio de Almeida Junior <jose.junior@andritz.com>
2023-09-12 10:20:24 -0300

    feat(bookings): add get booking by id query handler

---
+++ src/Bookify.Application/Bookings/GetBooking/BookingResponse.cs
@@ -0,0 +1,20 @@
+﻿using Bookify.Domain.Bookings;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+public sealed record BookingResponse(
+    Guid Id,
+    Guid UserId,
+    Guid ApartmentId,
+    BookingStatus Status,
+    decimal PriceAmount,
+    string PriceCurrency,
+    decimal CleaningFeeAmount,
+    string CleaningFeeCurrency,
+    decimal AmenitiesUpChargeAmount,
+    string AmenitiesUpChargeCurrency,
+    decimal TotalPriceAmount,
+    string TotalPriceCurrency,
+    DateOnly DurationStart,
+    DateOnly DurationEnd,
+    DateTime CreatedOnUtc);
