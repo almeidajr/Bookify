3b9545326a549bb4bf86f4d51bda147a3104fc91
Jose Rogerio de Almeida Junior <jose.junior@andritz.com>
2023-09-12 10:44:11 -0300

    feat(apartments): add search apartments query handler

---
+++ src/Bookify.Application/Apartments/SearchApartments/AddressResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Application.Apartments.SearchApartments;
+
+public sealed record AddressResponse(
+    string Country,
+    string State,
+    string ZipCode,
+    string City,
+    string Street);
---
+++ src/Bookify.Application/Apartments/SearchApartments/ApartmentResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookify.Application.Apartments.SearchApartments;
+
+public sealed record ApartmentResponse(
+    Guid Id,
+    string Name,
+    string Description,
+    decimal Price,
+    string Currency,
+    AddressResponse Address);
