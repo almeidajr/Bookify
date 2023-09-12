8ad87af53cfc6dbb34b4af24a778a24722f3cd8e
Jose Rogerio de Almeida Junior <jose.junior@andritz.com>
2023-09-12 17:03:40 -0300

    feat(infrastructure): add sql connection factory implementation

--- src/Bookify.Infrastructure/InfrastructureServiceCollectionExtensions.cs
+++ src/Bookify.Infrastructure/InfrastructureServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Data;
 using Bookify.Application.Abstractions.Email;
 using Bookify.Domain.Abstractions;
 using Bookify.Domain.Apartments;
@@ -6,8 +7,10 @@ using Bookify.Domain.Bookings;
 using Bookify.Domain.Reviews;
 using Bookify.Domain.Users;
 using Bookify.Infrastructure.Clock;
+using Bookify.Infrastructure.Data;
 using Bookify.Infrastructure.Email;
 using Bookify.Infrastructure.Repositories;
+using Dapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,9 +21,15 @@ public static class InfrastructureServiceCollectionExtensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("Database") ??
+                               throw new InvalidOperationException(
+                                   "Database connection string not found in configuration.");
+        SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
+
         return services
             .AddSingleton<IDateTimeProvider, DateTimeProvider>()
             .AddSingleton<IEmailService, EmailService>()
+            .AddSingleton<ISqlConnectionFactory>(new SqlConnectionFactory(connectionString))
             .AddDbContext<ApplicationDbContext>(options =>
                 options
                     .UseNpgsql(configuration.GetConnectionString("Database"))
