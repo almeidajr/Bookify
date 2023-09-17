e17cc60a8ebb8b31203d9fbc278ef2bd2b19d239
Jose Rogerio de Almeida Junior <jose.junior@andritz.com>
2023-09-17 14:29:34 -0300

    test(architecture): add architecture layer dependency tests
    Added tests to ensure that the Domain layer does not have dependencies on the Application and Infrastructure layers, and that the Application layer does not have a dependency on the Infrastructure layer

---
+++ tests/Bookify.ArchitectureTests/BaseTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Bookify.Application;
+using Bookify.Domain.Abstractions;
+using Bookify.Infrastructure;
+
+namespace Bookify.ArchitectureTests;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(IEntity).Assembly;
+
+    protected static readonly Assembly ApplicationAssembly =
+        typeof(ApplicationServiceCollectionExtensions).Assembly;
+
+    protected static readonly Assembly InfrastructureAssembly =
+        typeof(InfrastructureServiceCollectionExtensions).Assembly;
+}
---
+++ tests/Bookify.ArchitectureTests/LayerTests.cs
@@ -0,0 +1,40 @@
+using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Bookify.ArchitectureTests;
+
+public class LayerTests : BaseTest
+{
+    [Fact]
+    public void DomainLayer_ShouldNotHaveDependencyOnApplicationLayer()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(ApplicationAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DomainLayer_ShouldNotHaveDependencyOnInfrastructureLayer()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ApplicationLayer_ShouldNotHaveDependencyOnInfrastructureLayer()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
