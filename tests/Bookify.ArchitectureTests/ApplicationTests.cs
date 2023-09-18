0acaaeed736d35c96bfc983bf7fac492aac605d9
Jose Rogerio de Almeida Junior <jose.junior@andritz.com>
2023-09-18 20:03:34 -0300

    test(architecture): enforce naming conventions for handlers and validators in application layer

---
+++ tests/Bookify.ArchitectureTests/ApplicationTests.cs
@@ -0,0 +1,50 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using FluentAssertions;
+using FluentValidation;
+using NetArchTest.Rules;
+
+namespace Bookify.ArchitectureTests;
+
+public class ApplicationTests : BaseTest
+{
+    [Fact]
+    public void CommandHandler_ShouldHaveNameEndingWithCommandHandler()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .That()
+            .ImplementInterface(typeof(ICommandHandler<>))
+            .Or()
+            .ImplementInterface(typeof(ICommandHandler<,>))
+            .Should()
+            .HaveNameEndingWith("CommandHandler")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void QueryHandler_ShouldHaveNameEndingWithQueryHandler()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .That()
+            .ImplementInterface(typeof(IQueryHandler<,>))
+            .Should()
+            .HaveNameEndingWith("QueryHandler")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Validator_ShouldHaveNameEndingWithValidator()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .That()
+            .Inherit(typeof(AbstractValidator<>))
+            .Should()
+            .HaveNameEndingWith("Validator")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
