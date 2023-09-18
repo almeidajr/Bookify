51593fa8aa556e8419db61987438386feb015502
Jose Rogerio de Almeida Junior <jose.junior@andritz.com>
2023-09-18 20:00:30 -0300

    test(architecture): test: ensure domain events have postfix

---
+++ tests/Bookify.ArchitectureTests/DomainTests.cs
@@ -0,0 +1,21 @@
+﻿using Bookify.Domain.Abstractions;
+using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Bookify.ArchitectureTests;
+
+public class DomainTests : BaseTest
+{
+    [Fact]
+    public void DomainEvent_ShouldHaveDomainEventPostfix()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .That()
+            .ImplementInterface(typeof(IDomainEvent))
+            .Should()
+            .HaveNameEndingWith("DomainEvent")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
